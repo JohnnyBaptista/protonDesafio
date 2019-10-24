9bb8ee6a19f2dd996ca53fca7907a79828c39058
JohnnyBaptista <joaosbaptista@hotmail.com>
2019-10-24 10:01:35 -0300

    Correção no caminho do download

--- src/config/multer.js
+++ src/config/multer.js
@@ -6,7 +6,7 @@ module.exports = {
     dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'), //caminho que o arquivo ira percorer
     storage: multer.diskStorage({
         destination: (req, file, cb) => {
-            cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'));
+            cb(null, './tmp/uploads');
         },
         filename: (req, file, cb) => {
             crypto.randomBytes(16, (err, hash) => {
